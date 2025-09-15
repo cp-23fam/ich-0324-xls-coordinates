4c2bb23fbd38db07e51bca8183a438df0f7111ba
cp-23fam <fabian.marti@ceff.ch>
2025-09-15 08:08:01 +0000

    POC

---
+++ XlsCoordinateConverter-fam.ps1
@@ -0,0 +1,83 @@
+function SplitCell {
+    param(
+        [string] $Cell
+    )
+
+    if (-not ($Cell -match "^([A-Za-z]+)(\d+)$")) {
+        throw "Cellule non valide"
+    }
+
+    $index = 0
+    for ($i = 0; $i -lt $Cell.Length; $i++) {
+        $char = ([int]$Cell[$i]) / 1
+        if ($char -lt 65) {
+            $index = $i
+            break
+        }
+    }
+
+    $letters = $Cell.Substring(0, $index)
+    $numbers = [int]$Cell.Substring($index, $Cell.Length - $index)
+
+    return @($letters, $numbers)
+}
+
+function StringToNumberTable {
+    param(
+        [string] $String
+    )
+
+    $String = $String.ToUpper()
+    
+    $table = @()
+
+    for ($i = 0; $i -lt $String.Length; $i++) {
+        $table += $String[$i] / 1 - 64
+    }
+    
+    return $table
+}
+
+function NumberTableToRow {
+    param(
+        [System.Collections.ArrayList] $Table
+    )
+    
+    $number = 0;
+
+    for ($i = 0; $i -lt $Table.Count; $i++) {
+        $pow = ($Table.Count - $i - 1);
+
+        if ($pow -eq 0) {
+            $number += $Table[$i]
+        }
+        else {
+            $number += [Math]::Pow(26, $pow) * $Table[$i]
+        }
+    }
+
+    return $number
+}
+function ConvertFrom-XlsCoordinates {
+    param(
+        [string] $Cell
+    )
+
+    [array] $Splited = SplitCell -Cell $Cell
+    [array] $Tabled = StringToNumberTable -String $Splited[0]
+    [int] $Numbered = NumberTableToRow -Table $Tabled
+
+    [hashtable] $Table = @{
+        'Row'    = $Splited[1]
+        'Column' = $Numbered
+    }
+
+    return $Table
+}
+
+# Define Cell coordinates of start insertion
+[string] $grilleCelluleInsertion = 'D3'
+
+# Convert Cell coordinates from string to hashtable with Row and Column keys
+[hashtable] $grilleCoordinatesInsertion = ConvertFrom-XlsCoordinates -Cell $grilleCelluleInsertion
+Write-Host $grilleCoordinatesInsertion
